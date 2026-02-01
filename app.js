b18ce127e4dc685572d96999d18587c906b477cd
Heshmert <heshmert@protonmail.com>
2026-01-31 22:26:03 -0400

    re-orden de rutas

--- app.js
+++ app.js
@@ -15,12 +15,13 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
-app.use(express.json());
-
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use("/", routes);
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
 
 module.exports = app;
